56cb46ab2a7a997ba20b074e27689d270ad2fa8a
Ayush Gupta <ayush881gupta@gmail.com>
2022-12-07 01:06:56 +0530

    comments

--- terraform.tf
+++ terraform.tf
@@ -4,6 +4,6 @@
 ##################
 
 provider "aws"{
-  profile = "AP-account-4"
-  region  = "ap-south-1"
+  profile = "AP-account-4" #AWS Profile set in local for aws account athentication
+  region  = "ap-south-1" #AWS Region
 }
